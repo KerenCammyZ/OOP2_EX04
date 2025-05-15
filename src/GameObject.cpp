14aafc1c2eadd64b1b4b38373ebfff9eac23cdde
yissacharwe <96682093+yissacharwe@users.noreply.github.com>
2025-05-15 20:14:59 +0300

    Update GameObject constructor
    initialize m_position in member initializer list

--- src/GameObject.cpp
+++ src/GameObject.cpp
@@ -1,8 +1,8 @@
 #include "GameObject.h"
 
 GameObject::GameObject(sf::Vector2f position)
+	: m_position(position)
 {
-	m_position = position;
 	m_shape.setPosition(m_position);
 	m_shape.setFillColor(sf::Color::White);
 	m_shape.setOutlineThickness(1.f);
