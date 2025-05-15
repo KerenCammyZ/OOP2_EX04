0cdd1898e0e84ac6393ab604349d87d0915eb218
yissacharwe <96682093+yissacharwe@users.noreply.github.com>
2025-05-15 20:17:38 +0300

    GameObject setColor definition
    added missing definition for setColor declaration

--- src/GameObject.cpp
+++ src/GameObject.cpp
@@ -8,3 +8,8 @@ GameObject::GameObject(sf::Vector2f position)
 	m_shape.setOutlineThickness(1.f);
 	m_shape.setOutlineColor(sf::Color::Blue);
 }
+
+void GameObject::setColor(const sf::Color& color)
+{
+	m_shape.setFillColor(color);
+}
